790eef00d72d57a3e9a2ae3943de234219b6791f
José Lin <lin-jose@outlook.com>
2022-07-18 12:52:37 -0200

    Add calculation function and error notification

---
+++ app.js
@@ -0,0 +1,43 @@
+document.getElementById("loan-form").addEventListener("submit", calculateResults)
+
+function calculateResults(e) {
+  // UI elements
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthlyValue = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthlyValue)) {
+    monthlyPayment.value = monthlyValue.toFixed(2);
+    totalPayment.value = (monthlyValue * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthlyValue * calculatedPayments) - principal).toFixed(2);
+  } else {
+    showError("Please check your numbers")
+  }
+
+  e.preventDefault();
+}
+
+function showError(error) {
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+  const errorDiv = document.createElement("div");
+
+  errorDiv.className = "alert alert-danger";
+  errorDiv.textContent = error;
+
+  card.insertBefore(errorDiv, heading);
+
+  setTimeout(() => {
+    document.querySelector(".alert").remove();
+  }, 2000);
+}
